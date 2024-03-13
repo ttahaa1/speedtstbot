3d1be6e1b2de48e7b3724c3fc482eb46e4b06b28
speed <153678863+ttahaa1@users.noreply.github.com>
2024-03-13 17:02:53 +0200

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,75 @@
+from pytube import YouTube
+import os
+import telebot 
+import random
+from telebot import types
+from flask import Flask, request
+
+# تهيئة البوت
+bot = telebot.TeleBot('6634907418:AAHcYzt6-YZ1yjyHsUSL3HRAxsM50GoTEIU')
+
+# نقطة النهاية للويبهوك والتي يتصل بها Heroku
+server = Flask(__name__)
+
+# رابط التوكن الخاص بالبوت
+TOKEN = '6634907418:AAHcYzt6-YZ1yjyHsUSL3HRAxsM50GoTEIU'
+
+@bot.message_handler(commands=['start'])
+def message1(message):
+    id1 = str(message.from_user.id)
+    ty = types.InlineKeyboardButton(text='دخول البوت',callback_data='ty')
+    kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
+    bot.send_message(message.chat.id,'*اهلا بك في بوت تحميل من اليوتيوب*',parse_mode='markdown',reply_markup=kj)
+
+@bot.callback_query_handler(func=lambda call:True)
+def call(call):
+    if call.data =='ty':
+        nc = types.InlineKeyboardButton(text='تحميل فيديو',callback_data='nc')
+        cn = types.InlineKeyboardButton(text='تحميل مقطع صوتي',callback_data='cn')
+        ncc = types.InlineKeyboardMarkup(row_width=1)
+        ncc.add(nc,cn)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*اختار التحميل المناسب*',reply_markup=ncc,parse_mode='markdown')
+    elif call.data =='nc':
+        message = bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*ارسل الان رابط المقطع من فضلك*',parse_mode='markdown')
+        bot.register_next_step_handler(message,m2,message.id)
+
+def m2(message,id):
+    id1 = str(message.from_user.id)
+    me = str(message.text)
+    if ('https') in me :
+        ty = types.InlineKeyboardButton(text='مبرمج البوت',url='https://t.me/KOK0KK')
+        kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
+        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*جار التحميل الان..*',reply_markup=kj,parse_mode='markdown')
+        video_url = me
+        yt = YouTube(video_url)
+        video = yt.streams.first()
+        video.download()
+    
+        filem = video.default_filename
+     
+        u='qwertyuioplkjhgfdsazxcvbn'
+        rr = str(''.join(random.choice(u)for ii in range(4)))
+        namenew = f'{rr}.mp4'
+        os.rename(filem, namenew)
+        with open(namenew,'rb') as ad:
+            bot.send_audio(id1,ad,caption='*تم التحميل بنجاح*',parse_mode='markdown')
+            os.remove(filem)
+            os.remove(f'{rr}.mp3')   
+    else:
+        mi = types.InlineKeyboardButton(text='القائمة الرئسية',callback_data='ty')
+        mi1 = types.InlineKeyboardMarkup(row_width=2);mi1.add(mi)
+        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*عذرا ارسل رابط صحيح من فضلك*',parse_mode='markdown',reply_markup=mi1)
+
+@server.route("/" + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://your-heroku-app.herokuapp.com/" + TOKEN)
+    return "!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
