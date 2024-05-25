34e3c594ef039e0eb7fae0a73e18b30c40b44f06
speed <153678863+ttahaa1@users.noreply.github.com>
2024-05-26 00:04:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,16 @@ def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=welcome_message)
 
 def forward_message(update, context):
-    user_name = update.message.from_user.username
-    message = f"{user_name}: {update.message.text}"
-    context.bot.forward_message(chat_id=6264668799, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
-    reply_text = "تم إرسال رسالتك بنجاح وسأقوم بالرد عليك في أقرب وقت ممكن."
-    context.bot.send_message(chat_id=update.message.chat_id, text=reply_text)
+    user_id = update.message.from_user.id
+    forwarded_user_id = update.message.forward_from.id if update.message.forward_from else None
+    if forwarded_user_id and user_id != forwarded_user_id:
+        user_name = update.message.from_user.username
+        message = f"{user_name}: {update.message.text}"
+        context.bot.forward_message(chat_id='6264668799', from_chat_id=update.message.chat_id, message_id=update.message.message_id)
+        reply_text = "تم إرسال رسالتك بنجاح وسأقوم بالرد عليك في أقرب وقت ممكن."
+        context.bot.send_message(chat_id=update.message.chat_id, text=reply_text)
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text="عذراً، لا يمكن إعادة إرسال رسالتك.")
 
 start_handler = CommandHandler('start', start)
 updater.dispatcher.add_handler(start_handler)
