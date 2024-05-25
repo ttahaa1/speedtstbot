4303ef13363e35463e2f66fd2de4638ab383f32e
speed <153678863+ttahaa1@users.noreply.github.com>
2024-05-26 00:10:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-updater = Updater(token='YOUR_BOT_TOKEN', use_context=True)
+updater = Updater(token='6743547187:AAGfhT8wv-Z9Ds2NP_xItJs0Ud89o0qvyYE', use_context=True)
 
 def start(update, context):
     welcome_message = "أهلاً بك في بوت التواصل! أرسل رسالتك وسأقوم بالرد عليك فيما بعد."
@@ -12,7 +12,7 @@ def forward_message(update, context):
     if forwarded_user_id and user_id != forwarded_user_id:
         user_name = update.message.from_user.username
         message = f"{user_name}: {update.message.text}"
-        context.bot.forward_message(chat_id='YOUR_CHAT_ID', from_chat_id=update.message.chat_id, message_id=update.message.message_id)
+        context.bot.forward_message(chat_id='6264668799', from_chat_id=update.message.chat_id, message_id=update.message.message_id)
         reply_text = "تم إرسال رسالتك بنجاح وسأقوم بالرد عليك في أقرب وقت ممكن."
         context.bot.send_message(chat_id=forwarded_user_id, text=reply_text)
     else:
